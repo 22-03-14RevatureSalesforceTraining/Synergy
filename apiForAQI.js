f1ac3143a6af90b91e3c13b350876a955ea408fd
Reis Taylor <101010045+reisTaylor@users.noreply.github.com>
2022-03-22 14:40:45 -0700

    Update apiForAQI.js
    Updated file heading and documentation

--- apiForAQI.js
+++ apiForAQI.js
@@ -1,14 +1,23 @@
+/**apiForAQI.js
+*Reis Taylor
+*apiToken is variable issued from the public API
+*bKLatitude and bKLongitude are hard coded values  
+*which serve as arguments for the getData method
+**/
+
 const apiToken = "a3b6b6dc07b8740c6a7360fe6beedcd0a13865a1";
         const bKLatitude = 13.7563;
         const bKLongitude = 100.5018;
 
+/**data is both a variable in this method and a sub-array within
+*JSON object that I needed to acces to obtain both Bangkok's 
+*current AQI and it's name
+**/
         getData(bKLatitude, bKLongitude);
-        //  console.log(aqiWidgit);
         var GV_DATA = [];
         function getData(latitude, longitude) {
             let api = `https://api.waqi.info/feed/geo:
         ${latitude};${longitude}/?token=${apiToken}`;
-            //console.log("API", api);
             fetch(api)
                 .then(response => response.json())
                 .then(data => {
@@ -17,6 +26,9 @@ const apiToken = "a3b6b6dc07b8740c6a7360fe6beedcd0a13865a1";
                     showAQI()
                 });
         }
+
+//showAQI provides my .html file access to the values referenced
+//by GV_DATA.aqi and GV_DATA.name
         function showAQI() {
             document.getElementById("divAqi").innerHTML = GV_DATA.aqi;
             document.getElementById("divCityName").innerHTML = GV_DATA.city.name;
